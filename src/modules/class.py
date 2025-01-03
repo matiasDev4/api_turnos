91841541eb5594b6448d1e7007c31582f3c076bd
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-01-03 14:42:43 -0300

    modificacion archivo class.py

--- src/modules/class.py
+++ src/modules/class.py
@@ -1,13 +1,26 @@
 import datetime, time
 
-
-
 # Crear clase fecha
 # Crear clase hora
 
 class date_():
     def __init__(self, date: datetime):
-        pass
-
+        self.date = date
+    
+    
     def create_date(self):
-        pass
+        #Funcion para crear las fechas - almacenarlas en un array 
+        self.date_list = []
+        self.date_list.append(self.date)
+        return self.date_list
+
+class hours():
+    def __init__(self, hour: time):
+        self.hour = hour
+    
+    def create_hour(self):
+        #Funcion para crear las horas - almacenarlas en un array 
+        self.hour_list = []
+        self.hour_list.append(self.hour)
+        return self.hour_list
+
