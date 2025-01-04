0bcf815f6d32643d1cfb703378aea3a5f32f79b0
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-01-03 23:07:03 -0300

    rango de fecha agregado

--- src/modules/class.py
+++ src/modules/class.py
@@ -1,26 +1,29 @@
-import datetime, time
+import datetime
+from datetime import time, date
 
 # Crear clase fecha
 # Crear clase hora
 
-class date_():
-    def __init__(self, date: datetime):
-        self.date = date
-    
+class Date:
+    def __init__(self, date1: datetime.datetime, date2: datetime.datetime):
+        self.date1 = date1
+        self.date2 = date2
+        self.dateList = []
     
     def create_date(self):
+        
         #Funcion para crear las fechas - almacenarlas en un array 
-        self.date_list = []
-        self.date_list.append(self.date)
-        return self.date_list
+        #El usuario puede generar un rango de fechas a eleccion
+        #Asignar un minimo de 5 horarios distintos por fecha
 
-class hours():
-    def __init__(self, hour: time):
-        self.hour = hour
-    
-    def create_hour(self):
-        #Funcion para crear las horas - almacenarlas en un array 
-        self.hour_list = []
-        self.hour_list.append(self.hour)
-        return self.hour_list
+        if self.dateList == []: #-< Si el array esta vacio
+            while self.date1 <= self.date2: #-> Mientras la fecha 1 sea menor o igual a la fecha 2
+                if self.date1.weekday() < 5 and self.date1.weekday() != 0: #-> Salteo de dias lunes, Sabados y Domingos
+                    self.dateList.append(self.date1.strftime("%d-%m-%Y")) #-> Agrego las fechas al array
+                self.date1 = self.date1 + datetime.timedelta(days=1) #-> Sumo un dia a la fecha
+            return self.dateList
+
+
+print(Date(datetime.datetime(2021, 1, 1), datetime.datetime(2021, 2, 1)).create_date())
+
 
