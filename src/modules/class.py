31faf29d92ecabc9b06bb7a7ceddb9e4ab919074
matiasdev4 <matiasalvarezoaks@gmail.com>
2025-01-04 10:53:57 -0300

    modificacion class.py

--- src/modules/class.py
+++ src/modules/class.py
@@ -1,8 +1,11 @@
 import datetime
 from datetime import time, date
+import os
+import json
 
 # Crear clase fecha
 # Crear clase hora
+path = os.getcwd() 
 
 class Date:
     def __init__(self, date1: datetime.datetime, date2: datetime.datetime, hour1: datetime.time, hour2: datetime.time):
@@ -12,15 +15,8 @@ class Date:
         self.hour1 = hour1
         self.hour2 = hour2
         self.hourList = []
-        self.daysList = [
-            "Lunes",
-            "Martes",
-            "Miercoles",
-            "Jueves",
-            "Viernes",
-            "Sabado",
-            "Domingo"
-        ]
+        self.data = {}
+        self.data["dates"] = []
     
     def create_date(self):
         
@@ -41,9 +37,32 @@ class Date:
                 self.hour1 = (datetime.datetime.combine(date.today(), self.hour1) + datetime.timedelta(minutes=60)).time()
             #return self.hourList
         
+        if not os.path.exists(path + "/dates"):
+            os.makedirs(path + "/dates")
 
+        if not os.path.exists(path + "/dates/dates.json"):
+            with open (path + "/dates/dates.json", "w") as file:
+                self.data["dates"].append({"date": [str(x) for x in self.dateList], "hour": [str(x) for x in self.hourList]})
+                data_json = json.dumps(self.data, indent=4)
+                file.write(data_json)        
+        else:
+            with open (path + "/dates/dates.json") as file:
+                json_file = json.load(file)
+
+                json_file["dates"].append({"date": [str(x) for x in self.dateList], "hour": [str(x) for x in self.hourList]})
+
+            with open (path + "/dates/dates.json", "w") as outfile:
+
+                json.dump(json_file, outfile, indent=4)
+
+        return "Archivo creado con exito"
+
+        
+        
 
 
 print(Date(datetime.datetime(2021, 1, 1), datetime.datetime(2021, 1, 31), datetime.time(8, 0, 0), datetime.time(18, 0, 0)).create_date())
+print(Date(datetime.datetime(2021, 2, 1), datetime.datetime(2021, 2, 28), datetime.time(8, 0, 0), datetime.time(18, 0, 0)).create_date())
+print(Date(datetime.datetime(2021, 3, 1), datetime.datetime(2021, 3, 28), datetime.time(8, 0, 0), datetime.time(18, 0, 0)).create_date())
 
 
